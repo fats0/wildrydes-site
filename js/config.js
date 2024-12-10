e5e79e76b8c926c5465407d5d38f43b669901bb2
Farai_Chivasa <fariecm@gmail.com>
2024-12-10 01:12:05 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tFfwibrT7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4ddh44s59nigtd34p9bhqt6i41', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_56qQxvVs8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4qpbpnb6quj085f4n9lbavu2ve', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
